dbd7c4597a15fdbbdf9d1b8384e17e4f4a145a4a
John Oshalusi <oshalusijohn@gmail.com>
2021-09-28 17:34:17 +0100

    Initial commit

---
+++ src/main/scala/Demo/Demo.scala
@@ -0,0 +1,12 @@
+package Demo
+
+object Demo {
+  def main(args: Array[String]): Unit = {
+    val str: String = "welcome";
+    val numbers = List(1, 2, 3, 4)
+    for (num <- numbers) println(num)
+    /*def add(a: Int, b: Int): Int = a + b
+    println(add(10, 15))*/
+  }
+}
+
